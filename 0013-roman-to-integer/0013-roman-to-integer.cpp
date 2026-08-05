78023d75c11816fce1cb36a554ad6aeb1317f557
utkarsh007ut <utkarshsharma0701@gmail.com>
2026-08-05 21:21:24 +0530

    Time: 0 ms (100%), Space: 10.1 MB (87.27%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,46 @@
+class Solution {
+
+public:
+    int num(char c){
+            if(c=='I')
+            return 1;
+
+            else if(c=='V')
+            return 5;
+
+            else if(c=='X')
+            return 10;
+
+            else if(c=='L')
+            return 50;
+
+            else if(c=='C')
+            return 100;
+
+            else if(c=='D')
+            return 500;
+
+            else if(c=='M')
+            return 1000;
+
+            return 0;
+        }
+public:
+    int romanToInt(string s) {
+        
+
+        int ans=0,index=0;
+        while(index<s.size()-1){
+            if(num(s[index])<num(s[index+1])){
+                ans-=num(s[index]);
+            }
+            else
+            ans+=num(s[index]);
+
+            index++;
+        }
+        ans+=num(s[s.size()-1]);
+
+        return ans;
+    }
+};
