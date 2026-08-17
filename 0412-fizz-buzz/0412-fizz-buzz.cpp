16b0063c4016d5847aef96dafe4d6cac9ac35140
Utkarsh Sharma <utkarshsharma0701@gmail.com>
2026-08-17 14:31:14 +0530

    Time: 0 ms (100%), Space: 11.8 MB (48.16%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> res(n);
+        vector<string> res;
         for(int i=1;i<=n;i++){
             if(i%3==0 && i%5==0)
-            res[i-1]="FizzBuzz";
+            res.push_back("FizzBuzz");
             else if(i%3==0)
-            res[i-1]="Fizz";
+            res.push_back("Fizz");
             else if(i%5==0)
-            res[i-1]="Buzz";
+            res.push_back("Buzz");
             else
-            res[i-1]=to_string(i);
+            res.push_back(to_string(i));
         }
 
         return res;
