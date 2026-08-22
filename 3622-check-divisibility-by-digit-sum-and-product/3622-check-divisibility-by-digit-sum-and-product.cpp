96726b6d45948b729d04738152ff0f63b29d66fe
Utkarsh Sharma <utkarshsharma0701@gmail.com>
2026-08-22 12:09:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.55%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -4,11 +4,10 @@ public:
     int s=0,m=1,t=0;
     int l=n;
     while(n>0){
-        s+=n%10;
-        m*=n%10;
-        n/=10;
-
-    }
-    t+=(s+m); return (l%t==0);
+        s+=n%10;m*=n%10;n/=10;}
+    
+    t+=(s+m); 
+    
+    return (l%t==0);
     }
 };
