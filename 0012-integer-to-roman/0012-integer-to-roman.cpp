bf0782df7d682fddd19b7e1625b81141cdb43fa4
Utkarsh Sharma <utkarshsharma0701@gmail.com>
2026-08-28 18:54:59 +0530

    Time: 7 ms (28.37%), Space: 10.3 MB (72.78%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -7,14 +7,14 @@ class Solution {
         if(n==1)
         return "I";
 
-        else if(n==3)
-        return "III";
+        else if(n==4)
+        return "IV";
 
         else if(n==2)
         return "II";
 
-        else if(n==4)
-        return "IV";
+        else if(n==3)
+        return "III";
 
         else if(n==5)
         return "V";
