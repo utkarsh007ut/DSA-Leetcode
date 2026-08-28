1dd361bc91074ed64a2a20cbc65b4f4ad240aa1f
Utkarsh Sharma <utkarshsharma0701@gmail.com>
2026-08-28 18:44:37 +0530

    Time: 35 ms (36.35%), Space: 14.1 MB (74.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,6 +5,9 @@ public:
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target)
+                
+                
+                
                 return {i,j};
             }
         }
