0549e6292768a264aa2739b7c49e850739c6111a
Utkarsh Sharma <utkarshsharma0701@gmail.com>
2026-09-04 23:16:46 +0530

    Time: 0 ms (100%), Space: 11.7 MB (26.27%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -5,14 +5,17 @@ public:
     
     
     for (int i = n - 1; i >= 0; i--) {
+        //check if smaller than 9
         if (digits[i] < 9) {
             digits[i]++;   
             return digits;
         }
+
+        //if it is 9 then make it zero
         digits[i] = 0;    
     }
     
-    
+    //it all are 9 then add 1 to starting
     digits.insert(digits.begin(), 1);
     return digits;
         
